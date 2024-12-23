5e5742001fa2a9cdab8e3ddade5eb47997578e9d
Wouter Glorieux <wouter@valyrian.tech>
2024-12-23 16:55:45 +0100

    fix: add conda initialization to shell script

--- text-generation-webui/start.sh
+++ text-generation-webui/start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 echo "Starting Text Generation Web UI... (this might take a minute or two)"
+
+# Initialize conda for shell script
+eval "$(conda shell.bash hook)"
+echo "Conda initialized"
+
 conda activate /workspace/text-generation-webui/installer_files/env
 echo "Conda environment activated"
+
 cd /workspace/text-generation-webui
 echo "Changed directory to text-generation-webui"
+
 python one_click.py --listen --extensions openai
 echo "Started Text Generation Web UI"
 
