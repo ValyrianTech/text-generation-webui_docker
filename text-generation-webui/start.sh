410d73175c538ac0720d68865305245baee32875
Wouter Glorieux <wouter@valyrian.tech>
2024-12-23 15:27:28 +0100

    Update Dockerfile and start.sh to properly initialize conda environment

--- text-generation-webui/start.sh
+++ text-generation-webui/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Source conda
+. /opt/conda/etc/profile.d/conda.sh
+
+# Activate the environment
+conda activate /app/installer_files/env
+
+# Set environment variable to skip GPU choice
+export GPU_CHOICE="N"
+
+# Run the application
+python one_click.py
+
+# Keep container running
 while true; do
     sleep 1
 done
