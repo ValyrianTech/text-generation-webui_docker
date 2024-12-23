0a4d520b9093aa84c8889afb605bc91509fa9c19
Wouter Glorieux <wouter@valyrian.tech>
2024-12-23 16:50:57 +0100

    fix: change directory to text-generation-webui before running one_click.py

--- text-generation-webui/start.sh
+++ text-generation-webui/start.sh
@@ -2,6 +2,7 @@
 
 echo "Starting Text Generation Web UI... (this might take a minute or two)"
 conda activate /workspace/text-generation-webui/installer_files/env
+cd /workspace/text-generation-webui
 python one_click.py --listen --extensions openai
 
 # Keep container running
