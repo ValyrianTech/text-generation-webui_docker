f8ea4fe5ce8927ae58190ca8f9fd78cd718dee81
Wouter Glorieux <wouter@valyrian.tech>
2024-12-23 16:53:15 +0100

    feat: add descriptive echo statements for better logging

--- text-generation-webui/start.sh
+++ text-generation-webui/start.sh
@@ -2,8 +2,11 @@
 
 echo "Starting Text Generation Web UI... (this might take a minute or two)"
 conda activate /workspace/text-generation-webui/installer_files/env
+echo "Conda environment activated"
 cd /workspace/text-generation-webui
+echo "Changed directory to text-generation-webui"
 python one_click.py --listen --extensions openai
+echo "Started Text Generation Web UI"
 
 # Keep container running
 while true; do
