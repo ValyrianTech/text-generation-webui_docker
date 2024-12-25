5bc81c7774e63a22a44f7ba9e83b157ad7c16e5a
Wouter Glorieux <wouter@valyrian.tech>
2024-12-25 19:05:35 +0100

    Update textgen-on-workspace.sh script

--- text-generation-webui/textgen-on-workspace.sh
+++ text-generation-webui/textgen-on-workspace.sh
@@ -5,7 +5,9 @@ mkdir -p /workspace
 
 if [[ ! -d /workspace/text-generation-webui ]]; then
 	# If we don't already have /workspace/text-generation-webui, move it there
+	echo "Moving /root/text-generation-webui to /workspace for persistence, this might take a while..."
 	mv /root/text-generation-webui /workspace
+	echo "Done moving /root/text-generation-webui to /workspace"
 else
 	# otherwise delete the default text-generation-webui folder which is always re-created on pod start from the Docker
 	rm -rf /root/text-generation-webui
